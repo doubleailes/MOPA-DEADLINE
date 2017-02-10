d74c5da1df7df0b083557eada834e9463a585857
ChienLoutre <maurin.mansard@gmail.com>
2017-02-10 22:25:11 +0100

    Update deadline.py

--- deadline.py
+++ deadline.py
@@ -1,11 +1,21 @@
 import os, sys
-impor getpass
+import getpass
 
 # Get the group
 # check if username is the same as the deadline name.
 
-render_user = getpass.getuser()
-render_quality = ['HD_','LD_','TEST_']
 
-def launch_render():
-  print('Render launched')
+
+class deadlineMopa():
+  
+  def __init__(self):
+    self.user = getpass.getuser()
+    render_quality = ['HD_','LD_','TEST_']
+    self.params = [render_quality]
+    
+  def launchRender(self):
+    '''
+    Launch a render.
+    '''
+    print('Render launched by ' + self.user)
+    print('It was launched in ' + self.params[0][0])
