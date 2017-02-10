5cd7ccde3189917b9a7db33f63426a172132dc1a
ChienLoutre <maurin.mansard@gmail.com>
2017-02-10 22:05:27 +0100

    Create deadline.py

---
+++ deadline.py
@@ -0,0 +1,11 @@
+import os, sys
+impor getpass
+
+# Get the group
+# check if username is the same as the deadline name.
+
+render_user = getpass.getuser()
+render_quality = ['HD_','LD_','TEST_']
+
+def launch_render():
+  print('Render launched')
