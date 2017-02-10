43aaad483ca552307a294f01fe03dd244c806bbb
ChienLoutre <maurin.mansard@gmail.com>
2017-02-10 22:31:02 +0100

    Update deadline.py

--- deadline.py
+++ deadline.py
@@ -9,13 +9,15 @@ import getpass
 class deadlineMopa():
   
   def __init__(self):
-    self.user = getpass.getuser()
+    self.user = getpass.getuser().upper()
     render_quality = ['HD_','LD_','TEST_']
-    self.params = [render_quality]
+    self.params = render_quality
     
-  def launchRender(self):
+  def launchRender(self, qual=0):
     '''
     Launch a render.
+    qual is the render mode (HD,LD,TEST)
     '''
     print('Render launched by ' + self.user)
-    print('It was launched in ' + self.params[0][0])
+    print('It was launched in ' + self.params[qual])
+    print('Render Name => ' + self.params[qual] + self.user)
